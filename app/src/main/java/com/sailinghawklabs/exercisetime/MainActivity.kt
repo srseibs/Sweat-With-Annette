5a6b1bbc07f29938a681a5009cd8e88f5b69fedd
Mike Seibel <mike@SailingHawkLabs.com>
2022-11-25 15:06:05 -0800

    Circle Button changes

--- app/src/main/java/com/sailinghawklabs/exercisetime/MainActivity.kt
+++ app/src/main/java/com/sailinghawklabs/exercisetime/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sailinghawklabs.exercisetime.screens.welcome.StartSreen
 import com.sailinghawklabs.exercisetime.ui.theme.ExerciseTimeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ExerciseTimeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+
+                StartSreen()
             }
         }
     }
--- app/src/main/java/com/sailinghawklabs/exercisetime/screens/welcome/StartSreen.kt
+++ app/src/main/java/com/sailinghawklabs/exercisetime/screens/welcome/StartSreen.kt
@@ -2,37 +2,51 @@ package com.sailinghawklabs.exercisetime.screens.welcome
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.sailinghawklabs.exercisetime.R
+import com.sailinghawklabs.exercisetime.components.CircleButtton
 import com.sailinghawklabs.exercisetime.ui.theme.ExerciseTimeTheme
 
 @Composable
 fun StartSreen(
     modifier: Modifier = Modifier
 ) {
-    
+    StartScreenContent()
 }
 
 @Composable
 fun StartScreenContent(
     modifier: Modifier = Modifier
 ) {
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primaryContainer)
+            .background(MaterialTheme.colorScheme.primaryContainer),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
     ){
         Image(
             modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
             painter = painterResource(id = R.drawable.start_logo),
             contentDescription = "app logo")
+
+        CircleButtton(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            buttonText = "Start",
+            onClicked = { /*TODO*/ }
+        )
     }
 }
 
