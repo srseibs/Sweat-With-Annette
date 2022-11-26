9547417675454120363b0023ad02509f17c432b2
Mike Seibel <mike@SailingHawkLabs.com>
2022-11-25 18:32:09 -0800

    Starting Exercise Screen content

--- app/src/main/java/com/sailinghawklabs/exercisetime/navigation/Navigation.kt
+++ app/src/main/java/com/sailinghawklabs/exercisetime/navigation/Navigation.kt
@@ -1,10 +1,11 @@
 package com.sailinghawklabs.exercisetime.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.sailinghawklabs.exercisetime.screens.ExerciseScreen
+import com.sailinghawklabs.exercisetime.screens.exerciseScreen.ExerciseScreen
 import com.sailinghawklabs.exercisetime.screens.welcome.StartScreen
 
 
@@ -19,20 +20,22 @@ fun Navigation(
         composable(
             route = NavigationRoutes.StartScreen.route
         ) {
-            StartScreen(onStart = {
-                navController.navigate(NavigationRoutes.ExerciseScreen.route) {
-                    popUpTo(NavigationRoutes.StartScreen.route) {
-                        inclusive = true
-                    }
+            StartScreen(
+                onStart = {
+                    navController.navigate(NavigationRoutes.ExerciseScreen.route)
                 }
-            }
             )
         }
 
         composable(
             route = NavigationRoutes.ExerciseScreen.route
         ) {
-            ExerciseScreen()
+
+            ExerciseScreen(
+                goBack = {
+                        navController.popBackStack()
+                }
+            )
         }
 
     }
--- app/src/main/java/com/sailinghawklabs/exercisetime/screens/exerciseScreen/ExerciseScreen.kt
+++ app/src/main/java/com/sailinghawklabs/exercisetime/screens/exerciseScreen/ExerciseScreen.kt
@@ -1,12 +1,80 @@
-package com.sailinghawklabs.exercisetime.screens
+package com.sailinghawklabs.exercisetime.screens.exerciseScreen
 
+import android.widget.TextView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun ExerciseScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    goBack: () -> Unit,
 ) {
+    ExerciseScreenContent(modifier = modifier, goBack = goBack)
 
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExerciseScreenContent(
+    modifier: Modifier = Modifier,
+    goBack: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                title = {
+                    Text(
+                        text = "Sweat with Annette",
+                        style = MaterialTheme.typography.titleLarge
+                        )
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back arrow")
+                    }
+                }
+            )
+        },
+
+    ) {scaffoldPadding ->
+        Column(
+            modifier = modifier
+                .padding(scaffoldPadding)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "Get ready to start",
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.displaySmall,
+            )
+        }
+
+    }
 }
