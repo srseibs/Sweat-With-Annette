de018f4af31e6689a0be627df5096a4fe6d9c51c
Mike Seibel <mike@SailingHawkLabs.com>
2022-12-05 17:54:22 -0800

    Playing with progress indicator auto-sizing

--- app/src/main/java/com/sailinghawklabs/exercisetime/screens/exerciseScreen/components/NumberedProgressIndicator.kt
+++ app/src/main/java/com/sailinghawklabs/exercisetime/screens/exerciseScreen/components/NumberedProgressIndicator.kt
@@ -1,21 +1,30 @@
 package com.sailinghawklabs.exercisetime.screens.exerciseScreen.components
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_5
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_6P
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_7_2013
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -32,14 +41,19 @@ fun NumberedProgressIndicator(
         "NumberedProgressIndicator: $activeExerciseIndex of $numExercises"
     )
 
-    LazyRow(
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
+            .padding(horizontal = 16.dp)
+            .background(Color.Green),
+        horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        items(count = numExercises) { index ->
+
+        val highlightedDotSize = 32.dp
+        val normalDotSize = 28.dp
+
+        repeat(numExercises) { index ->
 
             val backgroundColor: Color
             val dotSize: Dp
@@ -49,39 +63,64 @@ fun NumberedProgressIndicator(
 
             if (index < activeExerciseIndex) {
                 backgroundColor = MaterialTheme.colorScheme.secondary
-                dotSize = 28.dp
+                dotSize = normalDotSize
                 borderColor = backgroundColor
                 textColor = MaterialTheme.colorScheme.onSecondary
             } else if (index == activeExerciseIndex) {
-                dotSize = 34.dp
+                dotSize = highlightedDotSize
                 backgroundColor = MaterialTheme.colorScheme.primaryContainer
                 borderColor = MaterialTheme.colorScheme.secondary
                 textColor = MaterialTheme.colorScheme.onPrimaryContainer
             } else { // index > activeExerciseIndex
                 backgroundColor = MaterialTheme.colorScheme.primary
-                dotSize = 28.dp
+                dotSize = normalDotSize
                 borderColor = backgroundColor
                 textColor = MaterialTheme.colorScheme.onPrimary
             }
 
             val dotValue = (index + 1).toString()
+
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .background(backgroundColor)
-
-//                    .clip(CircleShape)
-
-                    .border(
-                        width = 2.dp,
-                        color = borderColor,
-                    )
                     .size(dotSize)
-            ) {
+                    .background(backgroundColor, shape = CircleShape)
+                    .border(BorderStroke(2.dp, borderColor), shape = CircleShape)
+                    .layout() { measurable, constraints ->
+                        // Measure the composable
+                        val placeable = measurable.measure(constraints)
+
+                        //get the current max dimension to assign width=height
+                        val currentHeight = placeable.height
+                        val currentWidth = placeable.width
+                        val newDiameter = maxOf(currentHeight, currentWidth)
+
+                        //assign the dimension and the center position
+                        layout(newDiameter, newDiameter) {
+                            // Where the composable gets placed
+                            placeable.placeRelative(
+                                (newDiameter - currentWidth) / 2,
+                                (newDiameter - currentHeight) / 2
+                            )
+                        }
+                    }
+            ){
+
                 Text(
-                    modifier = Modifier.padding(0.dp),
+                    modifier = Modifier
+                        .padding(1.dp),
+//                        .drawBehind {
+//                            drawCircle(
+//                                color = borderColor,
+//                                radius = dotSize.toPx() + 1.dp.toPx(),
+//                            )
+//                            drawCircle(
+//                                color = backgroundColor,
+//                                radius = dotSize.toPx(),
+//                            )
+//                        },
                     text = dotValue,
-                    style = MaterialTheme.typography.labelSmall,
+                    style = MaterialTheme.typography.bodySmall,
                     textAlign = TextAlign.Center,
                     color = textColor,
                 )
@@ -91,7 +130,7 @@ fun NumberedProgressIndicator(
 }
 
 
-@Preview(showSystemUi = true)
+@Preview(showSystemUi = true, device = NEXUS_5)
 @Composable
 fun ExerciseProgressIndicatorPreview() {
     ExerciseTimeTheme {
