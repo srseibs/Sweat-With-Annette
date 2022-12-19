53f396e129ccd8c0cde3bc5b82cc1ed5d933100a
Mike Seibel <mike@SailingHawkLabs.com>
2022-12-18 16:48:28 -0800

    Ok, card decorations removed.

--- app/src/main/java/com/sailinghawklabs/sweatwithannette/util/dragdrop/DragDropColumn.kt
+++ app/src/main/java/com/sailinghawklabs/sweatwithannette/util/dragdrop/DragDropColumn.kt
@@ -1,5 +1,6 @@
 package com.sailinghawklabs.sweatwithannette.util.dragdrop
 
+import android.widget.Space
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
@@ -8,13 +9,21 @@ import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
@@ -79,8 +88,10 @@ fun <T : Any> DragDropColumn(
                 dragDropState = dragDropState,
                 index = index
             ) { isDragging ->
-                val elevation by animateDpAsState(if (isDragging) 4.dp else 0.dp)
-                Card(elevation = CardDefaults.cardElevation(defaultElevation = elevation)) {
+                Card(
+                    shape = RoundedCornerShape(0.dp),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+                ) {
                     itemContent(item)
                 }
             }
